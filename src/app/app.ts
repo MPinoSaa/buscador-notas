b15478bee88b8580bf86dc3229f32e348e635e27
MPinoSaa <matiaspinosaa@gmail.com>
2026-08-12 15:54:44 -0400

    Primer avance: CRUD con Firebase, SweetAlert y Dark Mode

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,142 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NotesService, Note } from './services/notes';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('buscador-notas');
+export class App implements OnInit{
+  searchTerm = '';
+  notes: Note[] = [];
+
+  selectedNote: Note | null = null;
+  isEditing = false;
+  isDarkMode = false;
+
+  constructor(
+    private notesService: NotesService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
+      const savedTheme = localStorage.getItem('darkMode');
+      if (savedTheme === 'true') {
+        this.isDarkMode = true;
+        document.body.classList.add('dark-body');
+    }
+  }
+
+    this.onSearch(); // Cargamos las notas que bajo el Firebase  
+    this.notesService.onDataChange = () => { // Se queda escuchando futuros cambios en tiempo real
+      this.onSearch();
+      this.cdr.detectChanges();
+    };
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
+      localStorage.setItem('darkMode', this.isDarkMode.toString());
+    }
+    if (this.isDarkMode) {
+      document.body.classList.add('dark-body');
+    } else {
+      document.body.classList.remove('dark-body');
+    }
+  }
+
+  onSearch() {
+    this.notes = this.notesService.searchNotes(this.searchTerm);
+  }
+
+  // Permite procesar los archivos .txt subidos
+  onFilesSelected(event: any) {
+    const files: FileList = event.target.files;
+    if (!files) return;
+    for (let i = 0; i < files.length; i++) { // Recorrer todos los archivos seleccionados
+      const file = files[i];
+      const reader = new FileReader();
+      // Cuando el archivo termina de leerse, ejecutamos esto:
+      reader.onload = async (e: any) => {
+        const newNote: Note = {
+          title: file.name.replace('.txt', ''), // Usamos el nombre del archivo sin la extensión
+          content: e.target.result // El contenido del .txt
+        };
+        await this.notesService.addNote(newNote);
+
+        Swal.fire({
+          toast: true,
+          position: 'top-end',
+          icon: 'success',
+          title: 'Nota cargada',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      };
+      reader.readAsText(file);
+    }
+    event.target.value = '';
+  }
+
+  // Vista detallada
+  openNote(note: Note) {
+    this.selectedNote = { ...note }; 
+    this.isEditing = false;
+  }
+
+  closeNote() {
+    this.selectedNote = null;
+    this.isEditing = false;
+    this.cdr.detectChanges();
+  }
+
+  toggleEdit() {
+    this.isEditing = true;
+  }
+
+  async saveNote() {
+    if (this.selectedNote && this.selectedNote.id) {
+      await this.notesService.updateNote(this.selectedNote.id, this.selectedNote.title, this.selectedNote.content);
+      this.isEditing = false;
+
+      Swal.fire({
+        icon: 'success',
+        title: '¡Guardado!',
+        text: 'La nota se ha actualizado correctamente.',
+        timer: 2000,
+        showConfirmButton: false
+      });
+    }
+  }
+
+  async deleteSelectedNote() {
+    if (this.selectedNote && this.selectedNote.id) {
+      const result = await Swal.fire({
+        title: '¿Estás seguro?',
+        text: "Esta acción no se puede deshacer.",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#ef4444',
+        cancelButtonColor: '#3b82f6',
+        confirmButtonText: 'Sí, eliminar',
+        cancelButtonText: 'Cancelar'
+      });
+
+      if (result.isConfirmed) {
+        await this.notesService.deleteNote(this.selectedNote.id);
+        this.closeNote();
+        Swal.fire(
+          '¡Eliminada!',
+          'Tu nota ha sido borrada.',
+          'success'
+        );
+      }
+    }
+  }
 }
---
+++ src/app/services/notes.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc } from 'firebase/firestore';
+
+export interface Note {
+  id?: string;
+  title: string;
+  content: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotesService {
+  private db;
+  private notesRef;
+  private notes: Note[] = [];
+  public onDataChange: () => void = () => {};
+
+  private firebaseConfig = {
+    apiKey: "AIzaSyBBa7pGw9FCFCs3_EdvNVWgEk-izd1oqQk",
+    authDomain: "buscador-notas.firebaseapp.com",
+    projectId: "buscador-notas",
+    storageBucket: "buscador-notas.firebasestorage.app",
+    messagingSenderId: "748370186114",
+    appId: "1:748370186114:web:23b95c89bea7e6cbe1099b"
+  };
+
+  constructor () {
+    const app = initializeApp(this.firebaseConfig); // Inicializar conexión al proyecto
+    this.db = getFirestore(app); // Conectar a la base de datos Firestore
+    this.notesRef = collection(this.db, 'notas'); // Apuntamos a una "colección" llamada 'notas'
+
+    // Escuchar la nube en tiempo real
+    onSnapshot(this.notesRef, (snapshot) => {
+      this.notes = snapshot.docs.map(doc => ({
+        id: doc.id,
+        title: doc.data()['title'],
+        content: doc.data()['content']
+      }));
+      this.onDataChange(); // Realiza la actualización visual
+    })
+  }
+
+  // Guardar notas en Firebase
+  async addNote(note: Note) {
+    await addDoc(this.notesRef, {
+      title: note.title,
+      content: note.content
+    });
+  }
+
+
+  // Búsqueda local sobre los datos ya cargados
+  searchNotes(query: string): Note[] {
+    if (!query.trim()) return this.notes; // Si no hay búsqueda, devuelve todo
+    const lowerQuery = query.toLowerCase(); // Filtra buscando coincidencias en el título o en el contenido
+    return this.notes.filter(note => 
+      note.title.toLowerCase().includes(lowerQuery) || 
+      note.content.toLowerCase().includes(lowerQuery)
+    );
+  }
+
+  // Actualizar las notas
+  async updateNote(id: string, title: string, content: string) {
+    const noteRef = doc(this.db, 'notas', id);
+    await updateDoc(noteRef, { title, content });
+  }
+
+  // Eliminar las notas
+  async deleteNote(id: string) {
+    const noteRef = doc(this.db, 'notas', id);
+    await deleteDoc(noteRef);
+  }
+}
